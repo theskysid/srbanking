593a8185ca338820150454c223b4bbc3effdb1ec
theskysid <23x10sid@gmail.com>
2025-02-09 19:38:53 +0530

    Local Database Connected

--- BackendBank/src/main/java/com/theskysid/backendbank/BackendBankApplication.java
+++ BackendBank/src/main/java/com/theskysid/backendbank/BackendBankApplication.java
@@ -2,12 +2,16 @@ package com.theskysid.backendbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BackendBankApplication {
 
     public static void main(String[] args) {
+
+        // ApplicationContext context = SpringApplication.run(BackendBankApplication.class, args); //stores beans in context which is of type ApplicationContext
         SpringApplication.run(BackendBankApplication.class, args);
+
     }
 
 }
