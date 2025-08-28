80e3884eebdb1e13031657cde9fc086729ce19a3
theskysid <23x10sid@gmail.com>
2025-08-29 01:04:59 +0530

    SpringBoot project initialized

--- backendbank/bank/src/main/java/com/theskysid/bank/entity/User.java
+++ backendbank/bank/src/main/java/com/theskysid/bank/entity/User.java
@@ -1,4 +1,16 @@
 package com.theskysid.bank.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private Long id;
+   private String username;
+   private String password;
 }
--- backendbank/bank/src/main/java/com/theskysid/bank/repository/UserRepository.java
+++ backendbank/bank/src/main/java/com/theskysid/bank/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.theskysid.bank.repository;
 
+import com.theskysid.bank.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
